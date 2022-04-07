ea1e34030ac326a6da6e4cf33ca69b25637a9df3
Husein Adenwala <huseinadenwala@gmail.com>
2022-04-07 15:23:20 -0500

    code added

---
+++ app.py
@@ -0,0 +1,7 @@
+ #!/usr/bin/env python
+
+def hello(Name):
+    return f"My Name is: {Name}"
+
+if __name__ == "__main__":
+    print(hello("Husein"))
---
+++ test_app.py
@@ -0,0 +1,4 @@
+from app import hello
+
+def test_hello():
+    assert "Husein" in hello("Husein")
