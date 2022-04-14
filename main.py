6c9fb3acdfef9b0f86f1aabca870b97adc86d3c8
Husein Adenwala <huseinadenwala@gmail.com>
2022-04-14 15:17:42 -0500

    GAE main code udpate

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    name = request.args.get('name', 'World')
-    return jsonify({'message': 'Hello {name}!'})
+    
+    return jsonify({'message': 'Hello World!'})
 
 
 if __name__ == "__main__":
