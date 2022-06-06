152290c171616b37544780ccc36eca1db81d8729
Husein Adenwala <huseinadenwala@gmail.com>
2022-06-05 20:38:40 -0500

    delete t.py

--- t.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-print(os.getenv('api_key'), os.getenv('api_secret_key'))
-print(os.getenv('access_token'), os.getenv('secret_access_token'))
